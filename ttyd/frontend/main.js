a95375c4a64d3982147ce25fb83be0b3c6401471
zongou <zongou@outlook.com>
2023-08-01 13:29:37 +0800

    Addimagee support, xterm-addon-image-0.4.2

--- ttyd/frontend/main.js
+++ ttyd/frontend/main.js
@@ -130,6 +130,21 @@ function start() {
     term.loadAddon(searchAddon);
     term.loadAddon(searchbarAddon);
 
+    // customize as needed (showing addon defaults)
+    // initialization
+    term.loadAddon(new ImageAddon.ImageAddon({
+        enableSizeReports: true,    // whether to enable CSI t reports (see below)
+        pixelLimit: 16777216,       // max. pixel size of a single image
+        sixelSupport: true,         // enable sixel support
+        sixelScrolling: true,       // whether to scroll on image output
+        sixelPaletteLimit: 256,     // initial sixel palette size
+        sixelSizeLimit: 25000000,   // size limit of a single sixel sequence
+        storageLimit: 128,          // FIFO storage limit in MB
+        showPlaceholder: true,      // whether to show a placeholder for evicted images
+        iipSupport: true,           // enable iTerm IIP support
+        iipSizeLimit: 20000000      // size limit of a single IIP sequence
+    }));
+
     // Binding Shortcuts
     document.addEventListener('keydown', function (event) {
         // show searchbar
